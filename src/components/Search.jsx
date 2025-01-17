22130a9ef693c1204efdb6782a4f5040ebc0c059
nurhaminator <nurmustapha02@gmail.com>
2025-01-17 15:49:18 +0800

    Removed default react template

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+    <form action="">
+        <input type="text" placeholder="Search for characters..." />
+        <button type="submit">Search</button>
+    </form>
+    </div>
+  )
+}
+
+export default Search
